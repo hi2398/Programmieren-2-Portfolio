1b4b462c25940e1a488aed93fc83117ab2a0d3a5
Pascal Hirt <79571006+hi2398@users.noreply.github.com>
2021-08-02 18:10:20 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# raylibstarter
+
+[![CMakeBuilds](https://github.com/chfhhd/raylibstarter/actions/workflows/cmake.yml/badge.svg)](https://github.com/chfhhd/raylibstarter/actions/workflows/cmake.yml)
+
+This raylib C++ project template for CMake is a modified fork of the raylibstarter template by Christoph Hahn (@chfhhd) [1], based on the window scale letterbox example [2] contributed by Anata (@anatagawa) and reviewed by Ramon Santamaria (@raysan5) and the CMakeLists.txt 
+example provided in the Raylib source code [3].
+
+The fork removes almost all premade classes to provide a cleaner start.
+
+[1] https://github.com/chfhhd/raylibstarter
+
+[1] https://github.com/raysan5/raylib/blob/master/examples/core/core_window_letterbox.c
+
+[2] https://github.com/raysan5/raylib/blob/master/projects/CMake/CMakeLists.txt
---
+++ src/main.cpp
@@ -0,0 +1,87 @@
+﻿#include <cstdlib>
+
+#include "raylib.h"
+
+#include "config.h"
+
+
+#define MAX(a, b) ((a)>(b)? (a) : (b))
+#define MIN(a, b) ((a)<(b)? (a) : (b))
+
+// Clamp Vector2 value with MIN and MAX and return a new vector2
+// NOTE: Required for virtual mouse, to clamp inside virtual game size
+Vector2 ClampValue(Vector2 value, Vector2 MIN, Vector2 MAX) {
+    Vector2 result = value;
+    result.x = (result.x > MAX.x) ? MAX.x : result.x;
+    result.x = (result.x < MIN.x) ? MIN.x : result.x;
+    result.y = (result.y > MAX.y) ? MAX.y : result.y;
+    result.y = (result.y < MIN.y) ? MIN.y : result.y;
+    return result;
+}
+
+
+int main() {
+    // Enable config flags for resizable window and vertical synchro
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT);
+    InitWindow(Game::ScreenWidth, Game::ScreenHeight, Game::PROJECT_NAME);
+    SetWindowMinSize(Game::ScreenWidth / 2, Game::ScreenHeight / 2);
+    // Set game to run at 60 frames-per-second
+    SetTargetFPS(60);
+
+    // Render texture initialization, used to hold the rendering result so we can easily resize it
+    RenderTexture2D target = LoadRenderTexture(Game::ScreenWidth, Game::ScreenHeight);
+    // Texture scale filter to use
+    SetTextureFilter(target.texture, FILTER_BILINEAR);
+
+#ifdef GAME_START_FULLSCREEN
+    ToggleFullscreen();
+#endif
+
+
+
+    // Main game loop
+    while (!WindowShouldClose()) // Detect window close button or ESC key
+    {
+        // Compute required framebuffer scaling
+        float scale = MIN((float) GetScreenWidth() / Game::ScreenWidth, (float) GetScreenHeight() / Game::ScreenHeight);
+
+        // Update virtual mouse (clamped mouse value behind game screen)
+        Vector2 mouse = GetMousePosition();
+        Vector2 virtualMouse = {0};
+        virtualMouse.x =
+                (mouse.x - (static_cast<float>(GetScreenWidth()) - (Game::ScreenWidth * scale)) * 0.5f) / scale;
+        virtualMouse.y =
+                (mouse.y - (static_cast<float>(GetScreenHeight()) - (Game::ScreenHeight * scale)) * 0.5f) / scale;
+        virtualMouse = ClampValue(virtualMouse, {0, 0}, {static_cast<float>(Game::ScreenWidth),
+                                                         static_cast<float>(Game::ScreenHeight)});
+
+
+        BeginDrawing();
+        ClearBackground(BLACK); // Letterbox color
+
+        // Draw everything in the render texture, note this will not be rendered on screen, yet
+        BeginTextureMode(target);
+
+        EndTextureMode();
+
+        // Draw RenderTexture2D to window, properly scaled
+        DrawTexturePro(target.texture,
+                       {0.0f, 0.0f, (float) target.texture.width, (float) -target.texture.height},
+                       {(static_cast<float>(GetScreenWidth()) - ((float) Game::ScreenWidth * scale)) * 0.5f,
+                        (static_cast<float>(GetScreenHeight()) - ((float) Game::ScreenHeight * scale)) * 0.5f,
+                        (float) Game::ScreenWidth * scale, (float) Game::ScreenHeight * scale},
+                       {0, 0}, 0.0f, WHITE);
+
+        EndDrawing();
+    } // Main game loop end
+
+    // De-Initialization here...
+
+    // Unload render texture
+    UnloadRenderTexture(target);
+
+    // Close window and OpenGL context
+    CloseWindow();
+
+    return EXIT_SUCCESS;
+}
